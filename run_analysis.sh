d0722a4375f068a37b94a9e1aae5a1d78d8d0478
samgans <antonzhiltsov2000@gmail.com>
2021-06-13 23:56:03 +0300

    Update bash script to enable testing

--- run_analysis.sh
+++ run_analysis.sh
@@ -4,7 +4,7 @@ zip -j tools ./src/tools/*
 zip configuration ./src/configuration/*
 
 if [ "$testing" == "-t" ]; then
-    echo "Testing is not implemented, but wait!"
+    python -m unittest discover tests/
 else
     spark-submit src/main.py --python-files tools.zip configuration.zip
 fi
